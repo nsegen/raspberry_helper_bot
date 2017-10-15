fb7a37a4bfb9192ea797c943d094365e0b355bf5
nsegen <segennikita@gmail.com>
2017-10-15 18:09:05 +0300

    change cups container

--- cups/start_cups.sh
+++ cups/start_cups.sh
@@ -3,7 +3,8 @@ set -e
 set -x
 
 if [ $(grep -ci $CUPS_USER_ADMIN /etc/shadow) -eq 0 ]; then
-    useradd $CUPS_USER_ADMIN --system -G root,lpadmin --no-create-home --password $(mkpasswd $CUPS_USER_PASSWORD)
+    adduser -H -S -G lpadmin $CUPS_USER_ADMIN 
+    echo $CUPS_USER_ADMIN:$CUPS_USER_PASSWORD | chpasswd
 fi
 
 exec /usr/sbin/cupsd -f
